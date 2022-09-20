7561bfdb0c4cb119a13fee5c148c3427d218d32e
Valentyna Kozulova <benedukvalia@gmail.com>
2022-09-20 10:07:45 +0300

    feat: cleaned and added benchmark

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,6 @@
 import App from './app';
-// import AuthRoute from '@routes/auth.route';
 import IndexRoute from './routes/index.route';
-// import UsersRoute from '@routes/users.route';
 import ArticleRote from './routes/articles.route';
-import validateEnv from './utils/validateEnv';
-
-validateEnv();
 
 const app = new App([new IndexRoute(), new ArticleRote()]);
 
